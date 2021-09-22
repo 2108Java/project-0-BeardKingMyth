3267c1a7bb3370166c9355caa109331519d34bee
Jacky Thomas <mythology216@gmail.com>
2021-09-22 15:11:55 -0500

    final

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -6,6 +6,9 @@ import com.revature.presentation.Presentation;
 import com.revature.presentation.PresentationImpl;
 import com.revature.service.Authenticator;
 import com.revature.service.AuthenticatorImpl;
+
+import java.util.Scanner;
+
 import com.revature.dao.AccountDao;
 import com.revature.dao.AccountDaoImpl;
 
@@ -14,11 +17,14 @@ public class MainDriver {
 	public static void main(String[] args) {
 		UserDao uDao = new UserDaoImpl();
 		AccountDao aDao = new AccountDaoImpl();
+		Scanner sc = new Scanner(System.in);
 		
 		Authenticator auth = new AuthenticatorImpl(uDao,aDao);
 		
 		
 		Presentation presentation = new PresentationImpl(auth);
+		
+		presentation.signInMenu(sc);
 		
 
 	}
